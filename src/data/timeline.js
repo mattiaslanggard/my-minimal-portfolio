8b2060bb10445a401e3e847cbd3adc548b554945
mattiaslanggardedupower <mattias.langgard@edupower.fi>
2022-11-22 19:26:24 +0200

    Initial Commit

---
+++ src/data/timeline.js
@@ -0,0 +1,30 @@
+export default[
+    {
+        year: 'Today',
+        title: 'Engineering Student',
+        duration: '4 years',
+        details: 
+            "I'm finishing up my last year at Novia UAS and looking for a project to work on as my bachelors thesis, and hopefully finding a workplace after my studies are done in the process.",
+    },
+    {
+        year: '2022',
+        title: 'Trainee at Edupower',
+        duration: '3½ months',
+        details: 
+            'I worked on automation of data logging and network protocols, database programming and planning of their implementation.',
+    },
+    {
+        year: '2019',
+        title: 'Baccalaureate at Gymnasiet i Petalax',
+        duration: '3 years',
+        details: 
+            'I took the baccalaureate with an M average in the 6 subjects I wrote in it.',
+    },
+    {
+        year: '2018 & 2019',
+        title: 'Summer worker at Björkback Gård',
+        duration: '3 months x 2',
+        details: 
+            'This was a versatile agricultural job where I was doing everything from construction work, animal husbandry to forest clearing.',
+    },
+]
